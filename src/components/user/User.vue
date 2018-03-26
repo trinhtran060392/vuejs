1e5dc66ee3a106522098127a1e1181e019cf2e3b
trinhtran <trinhtv@gemvietnam.com>
2018-03-26 19:02:07 +0700

    add params to url

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -9,19 +9,30 @@
         </v-flex>
       </v-layout>
     </v-container>
+    <div>
+      <carousel :autoplay="true">
+        <slide v-for="i in [1,2,3]" :key="`3${i}`"><span class="label">{{i}}</span></slide>
+      </carousel>
+    </div>
   </div>
 </template>
 
 <script>
   import UserService from './UserService'
+  import { Carousel, Slide } from 'vue-carousel'
   export default {
+    components: {
+      Carousel,
+      Slide
+    },
     data () {
       return {
         user: {}
       }
     },
     created () {
-      UserService.getUser(3).then((response) => {
+      let userId = this.$route.params.userId
+      UserService.getUser(userId).then((response) => {
         return response.body
       }).then((response) => {
         let result = response.data
@@ -33,5 +44,20 @@
 </script>
 
 <style scoped>
-  
+  .VueCarousel-slide {
+    position: relative;
+    background: #42b983;
+    color: #fff;
+    font-family: Arial;
+    font-size: 24px;
+    text-align: center;
+    min-height: 100px;
+  }
+
+  .label {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 </style>
