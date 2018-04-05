70d17f8a2ac404939bd220ffe147569159a83136
huongvx <huongvx@gemvietnam.com>
2018-04-05 22:11:07 +0700

    fix player init

--- src/components/player/Player.vue
+++ src/components/player/Player.vue
@@ -9,26 +9,21 @@
 <script>
   import VodService from '../vod/VodService'
   import Constant from '../shared/Constant'
+  import 'videojs-contrib-hls'
   import videojs from 'video.js'
-  // require('video.js')
   require('videojs-contrib-media-sources')
-  require('videojs-contrib-hls')
-  // let player
 
-  // let Hls = require('hls.js')
-  // let player
   export default {
     props: { vod: {} },
     data () {
       return {
         vodUrl: null,
-        isCompleted: false
+        isCompleted: false,
+        player: {}
       }
     },
     mounted () {
-      console.log('mount play')
-      let player = videojs('#video')
-      player.play()
+      this.player = videojs('video')
     },
     watch: {
       'vod' () {
@@ -43,35 +38,20 @@
                 this.vodUrl = 'http://' + addresses[i] + '/' + this.vod.vodLocator + '?AdaptiveType=HLS&VOD_RequestID=' + vodRequestId
               }
             }
-            // player.ready(() => {
-            //   player.src({
-            //     src: 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8',
-            //     type: 'application/x-mpegURL'
-            //   })
-            // })
-            // player.play()
-            // if (Hls.isSupported()) {
-            //   let video = document.getElementById('video')
-            //   let configHls = {
-            //     emeEnabled: true,
-            //     debug: true,
-            //     enableWorker: true,
-            //     widevineLicenseUrl: 'https://www.vietteltv.com.vn:8502'
-            //   }
-            //   console.log('https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8', this.vodUrl)
-            //   let hls = new Hls(configHls)
-            //   hls.loadSource(this.vodUrl)
-            //   hls.attachMedia(video)
-            //   video.play()
-            //   hls.on(Hls.Events.MANIFEST_PARSED, () => {
-            //     video.play()
-            //   })
-            // }
+            this.player.ready(() => {
+              this.player.src({
+                src: this.vodUrl,
+                type: 'application/x-mpegURL'
+              })
+            })
           }
         })
       }
     },
     destroyed () {
+      if (this.player) {
+        this.player.dispose()
+      }
     }
   }
 </script>
